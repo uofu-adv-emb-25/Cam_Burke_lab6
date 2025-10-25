fe4ffd0e87c47acaaea4ed144b78aab276df7f05
BurkeDambly <burkeadambly@gmail.com>
2025-10-25 15:57:05 -0600

    added the mutex test

--- test/test.c
+++ test/test.c
@@ -19,14 +19,23 @@
 #define TEST_TASK_STACK_SIZE     1024
 
 SemaphoreHandle_t share_semaphore;
+SemaphoreHandle_t mutex;
 
-int status_number; // 3 for higest task
+int status_number; // 3 for higest task, 2 for mid, 1 for lowest
+
+// Inversion log
 static volatile int log_arr[16];
-static volatile int log_index;;
+static volatile int log_index;
+
+// Mutex log
+static volatile int mutex_log_arr[16];
+static volatile int mutex_log_index;
 
 void setUp(void) {}
 
 void tearDown(void) {}
+
+
 
 void high_priority_thread(void *) {
     printf("High priority thread waiting for semaphore\n");
@@ -44,6 +53,7 @@ void high_priority_thread(void *) {
     vTaskDelete(NULL);
 }
 
+
 void middle_priority_thread(void *) {
     printf("Middle priority thread started\n");
     for(volatile int i = 0; i < 200000; i++){
@@ -55,6 +65,7 @@ void middle_priority_thread(void *) {
     }
     vTaskDelete(NULL);  
 }
+
 
 void low_priority_thread(void *) {
     printf("Low priority thread waiting for semaphore\n");
@@ -75,10 +86,9 @@ void low_priority_thread(void *) {
     }
     printf("Low priority thread finished\n");
     xSemaphoreGive(share_semaphore);
-    vTaskDelete(NULL);
-    
-    
+    vTaskDelete(NULL);   
 }
+
 
 void higher_thread(void) {
     
@@ -90,8 +100,7 @@ void higher_thread(void) {
 void middle_thread(void) {
     TaskHandle_t middle_T;  
     xTaskCreate(middle_priority_thread, "middle_thread", TEST_TASK_STACK_SIZE,
-                NULL, TASK_PRIORITY_MID, &middle_T);
-    
+                NULL, TASK_PRIORITY_MID, &middle_T);   
 }
 
 void lower_thread(void) {
@@ -101,43 +110,154 @@ void lower_thread(void) {
 }
 
 
+// Mutex semaphore version of the above threads
+
+void high_priority_thread_mutex(void *) {
+    printf("Mutex High Waiting\n");
+    xSemaphoreTake(mutex, portMAX_DELAY);
+    printf("Mutex High Start\n");
+    for(volatile int i = 0; i < 100000; i++){
+        status_number = 3;
+        if (mutex_log_arr[mutex_log_index] != status_number) {
+            mutex_log_index++;
+            mutex_log_arr[mutex_log_index] = status_number;
+        }
+    }  
+    printf("Mutex High Finished\n");
+    xSemaphoreGive(mutex);
+    vTaskDelete(NULL);
+}
+
+
+void middle_priority_thread_mutex(void *) {
+    printf("Mutex Middle Started\n");
+    for(volatile int i = 0; i < 200000; i++){
+        status_number = 2;
+        if (mutex_log_arr[mutex_log_index] != status_number) {
+            mutex_log_index++;
+            mutex_log_arr[mutex_log_index] = status_number;
+        }
+    }
+    vTaskDelete(NULL);  
+}
+
+
+void low_priority_thread_mutex(void *) {
+    printf("Low Mutex Waiting\n");
+
+    xSemaphoreTake(mutex, portMAX_DELAY);
+    printf("Low Mutex Started\n");
+    for(volatile int i = 0; i < 2000000; i++){
+        status_number = 1;
+
+        if (mutex_log_index == 0){
+           mutex_log_arr[mutex_log_index] = status_number;
+        }
+        
+        else if (mutex_log_arr[mutex_log_index] != status_number) {
+            mutex_log_index++;
+            mutex_log_arr[mutex_log_index] = status_number;
+        }
+    }
+    printf("Low Mutex Finished\n");
+    xSemaphoreGive(mutex);
+    vTaskDelete(NULL);   
+}
+
+
+void higher_thread_mutex(void) {
+    
+    TaskHandle_t higher_T_mutex;  
+    xTaskCreate(high_priority_thread_mutex, "higher_thread_mutex", TEST_TASK_STACK_SIZE,
+                NULL, TASK_PRIORITY_HIGH, &higher_T_mutex);
+}
+
+void middle_thread_mutex(void) {
+    TaskHandle_t middle_T_mutex;  
+    xTaskCreate(middle_priority_thread_mutex, "middle_thread_mutex", TEST_TASK_STACK_SIZE,
+                NULL, TASK_PRIORITY_MID, &middle_T_mutex);   
+}
+
+void lower_thread_mutex(void) {
+    TaskHandle_t lower_T_mutex;  
+    xTaskCreate(low_priority_thread_mutex, "lower_thread_mutex", TEST_TASK_STACK_SIZE,
+                NULL, TASK_PRIORITY_LOW, &lower_T_mutex);
+}
+
+
+// Test cases
 void test_priority_inversion(void) {
     int expected_log[] = {1, 2, 1, 3};
-    printf("Log index: %d\n", log_index);
-    for (int i = 0; i <= log_index; i++) {
+    for (int i = 0; i < 4; i++) {
         printf("Log arr[%d]: %d, expected: %d\n", i, log_arr[i], expected_log[i]);
         TEST_ASSERT_EQUAL_INT(expected_log[i], log_arr[i]);
     }
-    
-    
+}
+
+void test_priority_inversion_mutex(void) {
+    int expected_log[] = {1, 3, 2};
+    for (int i = 0; i < 3; i++) {
+        printf("Mutex Log arr[%d]: %d, expected: %d\n", i, mutex_log_arr[i], expected_log[i]);
+        TEST_ASSERT_EQUAL_INT(expected_log[i], mutex_log_arr[i]);
+    }
 }
  
+
 void supervisor(void *){
     while (1) {
+
+        // Test inversion using binary semaphore.
         share_semaphore = xSemaphoreCreateBinary();
         log_index = 0;
         status_number = 0;
+        for(int i = 0; i < 16; i++) {
+            log_arr[i] = 0;
+        }
         xSemaphoreGive(share_semaphore); // Initialize semaphore as available
-        printf("Start tests\n");
+        
 
         lower_thread();
         vTaskDelay(pdMS_TO_TICKS(10));
         higher_thread();
         vTaskDelay(pdMS_TO_TICKS(10));
         middle_thread();
-        vTaskDelay(pdMS_TO_TICKS(10));
         
-        vTaskDelay(pdMS_TO_TICKS(10000));
+        vTaskDelay(pdMS_TO_TICKS(2000));
+        
+
+        // Test inversion using mutex semaphore.
+        mutex = xSemaphoreCreateMutex(); 
+        mutex_log_index = 0;
+        status_number = 0;
+        xSemaphoreGive(mutex); 
+        for(int i = 0; i < 16; i++) {
+            mutex_log_arr[i] = 0;
+        }
+
+        lower_thread_mutex();
+        vTaskDelay(pdMS_TO_TICKS(10));
+        higher_thread_mutex();
+        vTaskDelay(pdMS_TO_TICKS(10));
+        middle_thread_mutex();
+        
+        vTaskDelay(pdMS_TO_TICKS(2000));
+
+        // Run tests
+        printf("Start tests\n");
+        vTaskDelay(pdMS_TO_TICKS(1000));
         UNITY_BEGIN();
-        printf("All done!\n");
+        
         RUN_TEST(test_priority_inversion);
+        RUN_TEST(test_priority_inversion_mutex);
+
+        printf("All done!\n");
         UNITY_END();
 
-        vTaskDelay(pdMS_TO_TICKS(5000));
-        
+        vTaskDelay(pdMS_TO_TICKS(5000));     
     }
-  
 }
+
+
 int main (void)
 {
     stdio_init_all();
@@ -146,7 +266,5 @@ int main (void)
     xTaskCreate(supervisor, "Supervisor_thread",
                 MAIN_TASK_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, NULL);
 
-    vTaskStartScheduler(); // This function should never return.
-    
-    
+    vTaskStartScheduler(); // This function should never return.  
 }
